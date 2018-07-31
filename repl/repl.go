1d209124efb602c76de330195302e7a333734f01
Radu Matei <matei.radu94@gmail.com>
2018-07-31 15:10:39 +0300

    Use parser in RPPL

--- repl/repl.go
+++ repl/repl.go
@@ -6,7 +6,7 @@ import (
 	"io"
 
 	"github.com/radu-matei/monkey/lexer"
-	"github.com/radu-matei/monkey/token"
+	"github.com/radu-matei/monkey/parser"
 )
 
 const prompt = ">>"
@@ -14,19 +14,28 @@ const prompt = ">>"
 // Start starts a REPL
 func Start(in io.Reader, out io.Writer) {
 	scanner := bufio.NewScanner(in)
-
 	for {
 		fmt.Printf(prompt)
-
 		scanned := scanner.Scan()
 		if !scanned {
 			return
 		}
 		line := scanner.Text()
 		l := lexer.New(line)
-
-		for tok := l.NextToken(); tok.Type != token.EOF; tok = l.NextToken() {
-			fmt.Printf("%+v\n", tok)
+		p := parser.New(l)
+		program := p.ParseProgram()
+		if len(p.Errors()) != 0 {
+			printParserErrors(out, p.Errors())
+			continue
 		}
+		io.WriteString(out, program.String())
+		io.WriteString(out, "\n")
+	}
+}
+
+func printParserErrors(out io.Writer, errors []string) {
+	io.WriteString(out, " parser errors:\n")
+	for _, msg := range errors {
+		io.WriteString(out, "\t"+msg+"\n")
 	}
 }
